e1edc131a6f53afbe64d1d711be6acd827df2d6d
sestens <santiago.estens@ttu.edu>
2016-04-20 12:00:36 -0500

    Add regex for removing css, html and javascript.

--- Url Positions/Module1.vb
+++ Url Positions/Module1.vb
@@ -1,6 +1,5 @@
 ﻿Imports System.IO
 Imports System.Text.RegularExpressions
-Imports HtmlAgilityPack
 
 Module Module1
 
@@ -9,64 +8,47 @@ Module Module1
   Dim crawlpath As String
   crawlpath = "C:\Users\sestens\Downloads\project\WebCrawlCompletedMar9_2016.txt"
   Dim text As String = File.ReadAllText(crawlpath)
-  Console.ReadLine()
-  Console.WriteLine("CRAWL TEST, GET URLS")
-  Console.WriteLine("first 0A appearence = " & text.IndexOf(vbLf)) '0A is the LF character that identifies an url in the crawl
+
   Dim otherRegex As New Regex("((https?|ftp|file)\://|www.)[A-Za-z0-9\.\-]+(/[A-Za-z0-9\?\&\=;\+!'\(\)\*\-\._~%]*)*", RegexOptions.IgnoreCase)
-  Dim findex, lindex As Integer
-  For Each m As Match In otherRegex.Matches(text)
-   findex = text.IndexOf(m.ToString)
-   lindex = m.ToString.Length + findex - 1
-   Console.WriteLine("match: " & m.ToString & "on position:" & findex & "-" & lindex)
-  Next
+  'Dim findex, lindex As Integer
+  'For Each m As Match In otherRegex.Matches(text)
+  'findex = text.IndexOf(m.ToString)
+  'lindex = m.ToString.Length + findex - 1
+  'Console.WriteLine("match: " & m.ToString & " on position:" & findex & "-" & lindex & " date:" & Today & " time:" & TimeOfDay)
+  'Next
+
+  Dim tagless As String = StripTags(text)
+  While tagless.IndexOf(vbLf) <> -1
 
 
-
-  'Dim inindex As Integer = text.IndexOf(vbLf)
-  'Dim substr As String = text.Substring(inindex + 2)
-  'Dim enindex As Integer = substr.IndexOf(ControlChars.Quote)
-  'Dim urlstr As String = substr.Substring(0, enindex)
-
-  'Dim begins, ends As Integer
-  'begins = text.IndexOf(urlstr)
-  'ends = urlstr.Length + begins - 1
-  ''Console.WriteLine("url: " & urlstr & " - starts in:" & begins & " ends in:" & ends)
-
-  'While (inindex > 0)
-
-  ' Console.WriteLine("url: " & urlstr & " - starts in:" & begins & " ends in:" & ends)
-  ' substr = substr.Substring(ends + 1) 'create a substring from the end of the url to the end of the file
-  ' inindex = substr.IndexOf(vbLf) 'find the next LF character
-  ' enindex = substr.IndexOf(ControlChars.Quote, 4)
-  ' urlstr = substr.Substring(inindex, enindex)
-  ' begins = text.IndexOf(urlstr)
-  ' ends = urlstr.Length + begins - 1
-
-  'End While
+  End While
 
   Console.ReadLine()
-
  End Sub
 
- Function findStartEnd(ByVal text As String)
-
-  Using outputfile As New StreamWriter("C:\Users\sestens\Downloads\project\output.txt")
-   Dim hrefRegex As New Regex("<A[^>]*?HREF\s*=\s*""([^""]+)""[^>]*?>([\s\S]*?)<\/A>", RegexOptions.IgnoreCase)
-   Dim otherRegex As New Regex("<a\s+href\s*=\s*""?([^"" >]+)""?>(.+)</a>", RegexOptions.IgnoreCase)
-   Dim startindex, length, endindex As Integer
-
-
-   For Each m As Match In otherRegex.Matches(text)
-    Console.WriteLine("There was a match")
-    startindex = m.Index
-    length = m.Length
-    endindex = length + startindex - 1
-    outputfile.WriteLine("url:=" & m.ToString() & " starting position = " & startindex & " ending position = " & endindex)
-
-   Next
-  End Using
-  Return 0
+ Function StripTags(ByVal html As String) As String
+  ' Remove HTML, javascript and css.
+  html = Regex.Replace(html, "<.*?>", "") 'removes html
+  html = Regex.Replace(html, "\<[^\>]*\>", "")
+  html = Regex.Replace(html, "-.*?>", "")
+  html = Regex.Replace(html, " [ ] ", "")
+  html = Regex.Replace(html, "{.*?}", "")
+  html = Regex.Replace(html, """", "")
+  html = Regex.Replace(html, "\|", "")
+  html = Regex.Replace(html, "\)", "")
+  html = Regex.Replace(html, "\;", "")
+  html = Regex.Replace(html, "\(", "")
+  html = Regex.Replace(html, "\?", "")
+  html = Regex.Replace(html, "\+", "")
+  html = Regex.Replace(html, "\=", "")
+  html = Regex.Replace(html, "\%", "")
+  html = Regex.Replace(html, "\&", "")
+  html = Regex.Replace(html, "\}", "")
+  html = Regex.Replace(html, "\(.*?\)", "")
+  html = Regex.Replace(html, "<style[^>]*>[\s\S]*?</style>", "") 'removes css
+  html = Regex.Replace(html, "<script.*?</script>", "", RegexOptions.IgnoreCase Or RegexOptions.Singleline) 'removes javasacript, using second regex found in beansofsoftware
+  html = Regex.Replace(html, "<!--[\s\S]*?-->", "")
+  html = Regex.Replace(html, "<[^>]*>", "")
+  Return html
  End Function
-
-
 End Module
