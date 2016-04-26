a3a70b84e34155d23fed4adf624ec7b6af8e042d
sestens <santiago.estens@ttu.edu>
2016-04-26 16:30:26 -0500

    Add code to remove stopwords from text.

--- Url Positions/Module1.vb
+++ Url Positions/Module1.vb
@@ -8,7 +8,7 @@ Module Module1
   Dim crawlfilepath As String
   crawlfilepath = "C:\Users\sestens\Downloads\project\WebCrawlCompletedMar9_2016.txt"
   Dim text As String = File.ReadAllText(crawlfilepath)
-  Dim taglesstxt As String = StripTags(text)
+  Dim taglesstxt As String = StripTags(text) 'remove tags from the crawl
   Dim allUrlsRegex As New Regex("((https?|ftp|file)\://|www.)[A-Za-z0-9\.\-]+(/[A-Za-z0-9\?\&\=;\+!'\(\)\*\-\._~%]*)*", RegexOptions.IgnoreCase)
   Dim vi, i, findex, lindex, lineFeedsCount As Integer
   Dim url As String = allUrlsRegex.Match(text).ToString
@@ -28,11 +28,18 @@ Module Module1
    Console.WriteLine(i & " match: " & url & " position:" & findex & "-" & lindex & " date:" & Today & " time: " & TimeOfDay)
   End While
   Console.ReadLine()
+  Dim txtcopy2 As String = textcopy
+  Dim keep, discard As StreamWriter
+  Dim stopWords As StreamReader
+  'keep = New StreamWriter(File.Create("C:\Users\sestens\Downloads\project\keep.txt"))
+  'discard = New StreamWriter(File.Create("C:\Users\sestens\Downloads\project\discard.txt"))
+  stopWords = File.OpenText("C:\Users\sestens\Downloads\project\stopwords_en.txt")
+  Do While stopWords.Peek >= 0
+   taglesstxt = Regex.Replace(taglesstxt, "\b" & stopWords.ReadLine() & "\b", " ") 'remove stopwords
+  Loop
 
-  Dim keep, discard, stopWords As FileStream
-  keep = New FileStream("C:\Users\sestens\Downloads\project\keep.txt", FileMode.Create, FileAccess.ReadWrite)
-  discard = New FileStream("C:\Users\sestens\Downloads\project\discard.txt", FileMode.Create, FileAccess.ReadWrite)
-  stopWords = New FileStream("C:\Users\sestens\Downloads\project\stopwords_en.txt", FileMode.Open, FileAccess.Read)
+  Console.Write(taglesstxt)
+  Console.ReadLine()
 
 
  End Sub
