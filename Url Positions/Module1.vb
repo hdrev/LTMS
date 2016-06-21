be7c3f4dd23437b071f172aff0e520fe536014e9
sestens <santiago.estens@ttu.edu>
2016-06-21 17:00:24 -0500

    Change keep file format to include tabs.

--- Url Positions/Module1.vb
+++ Url Positions/Module1.vb
@@ -7,17 +7,17 @@ Module Module1
 
   Dim crawlfilepath As String
   crawlfilepath = "C:\Users\sestens\Downloads\project\WebCrawlCompletedMar9_2016.txt"
-  Dim text As String = File.ReadAllText(crawlfilepath)
+  Dim text As String = File.ReadAllText(crawlfilepath) 'read the craw file
   Dim taglesstxt As String = StripTags(text) 'remove tags from the crawl
-  Dim allUrlsRegex As New Regex("((https?|ftp|file)\://|www.)[A-Za-z0-9\.\-]+(/[A-Za-z0-9\?\&\=;\+!'\(\)\*\-\._~%]*)*", RegexOptions.IgnoreCase)
+  Dim allUrlsRegex As New Regex("((https?|ftp|file)\://|www.)[A-Za-z0-9\.\-]+(/[A-Za-z0-9\?\&\=;\+!'\(\)\*\-\._~%]*)*", RegexOptions.IgnoreCase) 'regex to match urls
   Dim vi, i, findex, lindex, lineFeedsCount As Integer
   Dim url As String = allUrlsRegex.Match(text).ToString
-  Dim textcopy As String = text
+  Dim textcopy As String = text 'make a copy of the crawl file
   i = 1
   findex = text.IndexOf(url)
   lindex = url.Length + findex - 1
   Console.WriteLine(i & " match: " & url & " position:" & findex & "-" & lindex & " date:" & Today & " time:" & TimeOfDay)
-  lineFeedsCount = countLf(text)
+  lineFeedsCount = countLf(text) 'count # of line feed characters which identify number of urls
   While i <> lineFeedsCount
    vi = text.IndexOf(vbLf)
    url = allUrlsRegex.Match(text, vi).ToString
@@ -25,16 +25,16 @@ Module Module1
    i = i + 1
    findex = textcopy.IndexOf(url)
    lindex = url.Length + findex - 1
-   Console.WriteLine(i & " match: " & url & " position:" & findex & "-" & lindex & " date:" & Today & " time: " & TimeOfDay)
+   Console.WriteLine(i & " match: " & url & " position:" & findex & "-" & lindex & " date:" & Today & " time: " & TimeOfDay) 'output into console the url and its details
   End While
   Console.ReadLine()
   Dim txtcopy2 As String = textcopy
   Dim line As String
   Dim keep, discard As StreamWriter
   Dim stopWords As StreamReader
-  keep = New StreamWriter(File.Create("C:\Users\sestens\Downloads\project\keep.txt"))
-  discard = New StreamWriter(File.Create("C:\Users\sestens\Downloads\project\discard.txt"))
-  stopWords = File.OpenText("C:\Users\sestens\Downloads\project\stopwords_en.txt")
+  keep = New StreamWriter(File.Create("C:\Users\sestens\Downloads\project\keep.txt")) 'create a keep words file
+  discard = New StreamWriter(File.Create("C:\Users\sestens\Downloads\project\discard.txt")) 'create a discard words file
+  stopWords = File.OpenText("C:\Users\sestens\Downloads\project\stopwords_en.txt") 'open the stopwords file
   Dim lineregex As Regex
   Dim matches As MatchCollection
   Do While stopWords.Peek >= 0
@@ -50,10 +50,14 @@ Module Module1
   Dim wordregex As New Regex("\b(\S)+\b") 'regex to match words in strings
   matches = wordregex.Matches(taglesstxt)
   For Each match As Match In matches
-   keep.WriteLine(match.ToString & " length:" & match.ToString.Length & " start:" & textcopy.IndexOf(match.ToString) & " date:" & Today & " time:" & TimeOfDay) 'start position relative to textcopy
+   'write word in file
+   keep.WriteLine(match.ToString & vbTab & vbTab & "length:" & match.ToString.Length & vbTab & "start:" & textcopy.IndexOf(match.ToString) & vbTab & "date:" & Today & vbTab & vbTab & "time: " & TimeOfDay) 'start position relative to textcopy
   Next
 
-  Console.ReadLine()
+  'close text files
+  keep.Close()
+  discard.Close()
+  stopWords.Close()
 
 
  End Sub
